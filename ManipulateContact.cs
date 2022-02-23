d4ef6b94b5433f498626481292dbb48ac2e21cb9
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 20:59:13 +0530

    Add project files.

---
+++ ManipulateContact.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class ManipulateContact
+    {
+        List<ContactDetails> AddressBookList = new List<ContactDetails>();
+        public void AddingContact
+            (
+            string Name,
+            string Address,
+            string City,
+            string State,
+            string ZipCode,
+            string PhoneNumber,
+            string Email
+            )
+        {
+            AddressBookList.Add
+                (new ContactDetails()
+                {
+                    Name = Name,
+                    Address = Address,
+                    City = City,
+                    State = State,
+                    ZipCode = ZipCode,
+                    PhoneNumber = PhoneNumber,
+                    Email = Email
+                }
+            );
+            Console.WriteLine($"{Name}'s contact is added______");
+        }
+
+
+        public void EditingContact(string Name)
+        {
+            foreach (var contact in AddressBookList)
+            {
+                if (contact.Name.Contains(Name))
+                {
+                    /*Console.WriteLine("plz provide new ");
+                    contact.Address = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.City = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.State = Console.ReadLine();
+                    Console.WriteLine("plz provide new ");
+                    contact.ZipCode = Console.ReadLine();*/
+                    Console.WriteLine("plz enter new phone number");
+                    contact.PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("plz enter new email id");
+                    contact.Email = Console.ReadLine();
+                    Console.WriteLine($"{Name}'s Contact is edited______");
+                    break;
+                }
+            }
+        }
+
+        public void DeletingContact(string Name)
+        {
+            foreach (var contact in AddressBookList)
+            {
+                if (contact.Name.Contains(Name))
+                {
+                    AddressBookList.Remove(contact);
+                    Console.WriteLine($"{Name}'s Contact is deleted______");
+                    break;
+                }
+            }
+        }
+    }
+
+    internal class ContactDetails
+    {
+        public ContactDetails()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        public static ManipulateContact manipulate = new ManipulateContact();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to address book system");
+
+            AddContact();
+            EditContact();
+            DeleteContact();
+        }
+
+        public static void AddContact()
+        {
+            Console.WriteLine("how many contacts do you want to add?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("enter name");
+                string Name = Console.ReadLine();
+                Console.WriteLine("enter address");
+                string Address = Console.ReadLine();
+                Console.WriteLine("enter city");
+                string City = Console.ReadLine();
+                Console.WriteLine("enter state");
+                string State = Console.ReadLine();
+                Console.WriteLine("enter zip code");
+                string ZipCode = Console.ReadLine();
+                Console.WriteLine("enter phone number");
+                string PhoneNumber = Console.ReadLine();
+                Console.WriteLine("enter email id");
+                string Email = Console.ReadLine();
+                ContactDetails detail = new ContactDetails();
+                manipulate.AddingContact(
+                    detail.Name = Name,
+                    detail.Address = Address,
+                    detail.City = City,
+                    detail.State = State,
+                    detail.ZipCode = ZipCode,
+                    detail.PhoneNumber = PhoneNumber,
+                    detail.Email = Email
+                    );
+            }
+        }
+
+        public static void EditContact()
+        {
+            Console.WriteLine("enter name you want edit");
+            string Name = Console.ReadLine();
+            manipulate.EditingContact(Name);
+        }
+        public static void DeleteContact()
+        {
+            Console.WriteLine("enter name you want delete");
+            string Name = Console.ReadLine();
+            manipulate.DeletingContact(Name);
+        }
+    }
+}
