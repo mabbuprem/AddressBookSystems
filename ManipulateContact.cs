82bbd333bb9ed837cc485f64f7d008fecd87a446
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 21:02:51 +0530

    cteate contact

--- ManipulateContact.cs
+++ ManipulateContact.cs
@@ -32,45 +32,7 @@ namespace AddressBookSystem
                     Email = Email
                 }
             );
-            Console.WriteLine($"{Name}'s contact is added______");
-        }
-
-
-        public void EditingContact(string Name)
-        {
-            foreach (var contact in AddressBookList)
-            {
-                if (contact.Name.Contains(Name))
-                {
-                    /*Console.WriteLine("plz provide new ");
-                    contact.Address = Console.ReadLine();
-                    Console.WriteLine("plz provide new ");
-                    contact.City = Console.ReadLine();
-                    Console.WriteLine("plz provide new ");
-                    contact.State = Console.ReadLine();
-                    Console.WriteLine("plz provide new ");
-                    contact.ZipCode = Console.ReadLine();*/
-                    Console.WriteLine("plz enter new phone number");
-                    contact.PhoneNumber = Console.ReadLine();
-                    Console.WriteLine("plz enter new email id");
-                    contact.Email = Console.ReadLine();
-                    Console.WriteLine($"{Name}'s Contact is edited______");
-                    break;
-                }
-            }
-        }
-
-        public void DeletingContact(string Name)
-        {
-            foreach (var contact in AddressBookList)
-            {
-                if (contact.Name.Contains(Name))
-                {
-                    AddressBookList.Remove(contact);
-                    Console.WriteLine($"{Name}'s Contact is deleted______");
-                    break;
-                }
-            }
+            Console.WriteLine($"{Name} is added");
         }
     }
 
